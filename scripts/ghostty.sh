84400392d176d03255b33b4c16eec11acf1bf5c7
atrskv <legaterayne@gmail.com>
2026-05-30 22:20:45 +0300

    update ghostty setup

--- scripts/ghostty.sh
+++ scripts/ghostty.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
-
 set -e
 
-sudo apt install -y curl gpg lsb-release
+echo "Installing Ghostty..."
 
-curl -fsSL https://debian.griffo.io/archive.key |
-  sudo gpg --dearmor -o /usr/share/keyrings/debian-griffo-io.gpg
+sudo curl -fsSL https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc |
+  sudo gpg --dearmor --yes -o /usr/share/keyrings/debian.griffo.io.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/debian-griffo-io.gpg] \
-https://debian.griffo.io/debian/ \
-$(lsb_release -cs) main" |
-  sudo tee /etc/apt/sources.list.d/debian-griffo-io.list
+echo "deb [signed-by=/usr/share/keyrings/debian.griffo.io.gpg] https://debian.griffo.io/apt $(lsb_release -sc) main" |
+  sudo tee /etc/apt/sources.list.d/debian.griffo.io.list
 
 sudo apt update
 sudo apt install -y ghostty
+
+echo "Ghostty installed"
