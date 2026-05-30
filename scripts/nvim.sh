e6b9d603283323dc69664131ca8adb06118cbf16
atrskv <legaterayne@gmail.com>
2026-05-30 21:05:41 +0300

    update nvim setup

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,21 +1,35 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
 
+echo "Installing Neovim..."
+
+cd /tmp
+
+if [ -f nvim-linux-x86_64.tar.gz ]; then
+  echo "Removing old Neovim archive..."
+  rm nvim-linux-x86_64.tar.gz
+fi
+
+if [ -d nvim-linux-x86_64 ]; then
+  echo "Removing old extracted Neovim directory..."
+  rm -rf nvim-linux-x86_64
+fi
+
 echo "Downloading Neovim..."
-wget -q --show-progress https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+wget -q --show-progress \
+  https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 
 echo "Extracting..."
 tar -xf nvim-linux-x86_64.tar.gz
 
 echo "Moving to ~/nvim..."
-mv nvim-linux-x86_64 ~/nvim
+rm -rf "$HOME/nvim"
+mv nvim-linux-x86_64 "$HOME/nvim"
 
-echo "Creating symlink in /usr/local/bin (requires sudo)..."
-sudo ln -sf ~/nvim/bin/nvim /usr/local/bin/nvim
+echo "Creating symlink in /usr/local/bin..."
+sudo ln -sf "$HOME/nvim/bin/nvim" /usr/local/bin/nvim
 
-echo "Installing LazyVim..."
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+rm -f nvim-linux-x86_64.tar.gz
 
 echo "Done"
+nvim --version | head -n 1
