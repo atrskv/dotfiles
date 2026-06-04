7032f0b5c24bd46b2822eabba94b48df23c693b9
atrskv <legaterayne@gmail.com>
2026-06-04 13:42:00 +0300

    comment mirrors

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -16,20 +16,20 @@ fi
 NEW_SOURCES="$(
   cat <<'EOF'
 # Main
-# deb https://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
-deb https://mirror.yandex.ru/debian/ trixie main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+# deb https://mirror.yandex.ru/debian/ trixie main contrib non-free non-free-firmware
 
 # Updates
-# deb https://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
-deb https://mirror.yandex.ru/debian/ trixie-updates main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
+# deb https://mirror.yandex.ru/debian/ trixie-updates main contrib non-free non-free-firmware
 
 # Backports (apt install -t trixie-backports <>)
-# deb https://deb.debian.org/debian/ trixie-backports main contrib non-free non-free-firmware
-deb https://mirror.yandex.ru/debian/ trixie-backports main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian/ trixie-backports main contrib non-free non-free-firmware
+# deb https://mirror.yandex.ru/debian/ trixie-backports main contrib non-free non-free-firmware
 
 # Security
-# deb https://security.debian.org/debian-security/ trixie-security main contrib non-free non-free-firmware
-deb https://mirror.yandex.ru/debian-security/ trixie-security main contrib non-free non-free-firmware
+deb https://security.debian.org/debian-security/ trixie-security main contrib non-free non-free-firmware
+# deb https://mirror.yandex.ru/debian-security/ trixie-security main contrib non-free non-free-firmware
 EOF
 )"
 
